686dc7dc0b62e0c98fc80c7a1abb19dd5d0a4d1b
123Masha456 <xmail99@yandex.ru>
2023-08-09 05:31:25 -0700

    hw basket spring

--- src/main/java/pro/sky/basket/dto/Basket.java
+++
@@ -1,5 +0,0 @@
-package pro.sky.basket.dto;
-
-public class Basket {
-
-}
--- src/main/java/pro/sky/basket/service/BasketServiceImpl.java
+++ src/main/java/pro/sky/basket/service/BasketServiceImpl.java
@@ -1,4 +1,18 @@
 package pro.sky.basket.service;
+import java.util.List;
 
 public class BasketServiceImpl implements BasketService{
+    private final Basket basket;
+    public BasketServiceImpl(Basket basket){
+        this.basket = basket;
+    }
+    @Override
+    public void add(List<Long> items) {
+        items.forEach(item -> basket.add(item));
+    }
+
+    @Override
+    public List<Long> get() {
+        return basket.getItems();
+    }
 }
